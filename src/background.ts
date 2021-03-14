2481ed34a3c258bba0699df5e8813a940b2e3beb
Saad <saadmasood96@hotmail.com>
2021-03-14 12:29:13 -0400

    move helper functions into Helpers folder

--- src/background.ts
+++ src/background.ts
@@ -1,28 +1,7 @@
-import { AUTH_FAIL, AUTH_SUCCESS, LOGIN, Message } from "./types";
-import * as URLParse from 'url-parse';
-
-let STATE = '';
+import { LOGIN, Message } from "./types";
+import { getSpotifyAuthUrl, onAuthRedirect } from "./Helpers/auth";
 
 chrome.runtime.onMessage.addListener(chromeMessageHandler);
-
-function getSpotifyAuthUrl(): string {
-  STATE = encodeURIComponent('meet' + Math.random().toString(36).substring(2, 15));
-  const CLIENT_ID = 'f5a2c30e102140d0970820bcd154dba8';
-  const RESPONSE_TYPE = encodeURIComponent('token');
-  const REDIRECT_URI = encodeURIComponent('https://opdbejkeklgoefcmfeaehnnhpnnnnpii.chromiumapp.org/');
-  const SCOPE = encodeURIComponent([
-    'user-read-email',
-    'playlist-modify-private',
-    'user-library-modify'
-  ].join(' '));
-
-  const baseUrl = 'https://accounts.spotify.com/authorize';
-  const queryString =
-    `client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URI}&state=${STATE}&scope=${SCOPE}`;
-
-  const spotifyAuthUrl = `${baseUrl}?${queryString}`;
-  return spotifyAuthUrl;
-}
 
 function chromeMessageHandler(
   message: Message,
@@ -31,53 +10,14 @@ function chromeMessageHandler(
 ) {
   switch (message.type) {
     case LOGIN:
-        chrome.identity.launchWebAuthFlow(
-          { url: getSpotifyAuthUrl(), interactive: true },
-          (redirectUrl) => onAuthRedirect(sendResponse, redirectUrl)
-        );
+      const STATE = encodeURIComponent('meet' + Math.random().toString(36).substring(2, 15));
+      chrome.identity.launchWebAuthFlow(
+        { url: getSpotifyAuthUrl(STATE), interactive: true },
+        (redirectUrl) => onAuthRedirect(sendResponse, STATE, redirectUrl)
+      );
       break;
     default:
       break;
   }
   return true;
-}
-
-function onAuthRedirect(
-  sendResponse: (message?: Message) => void,
-  redirectUrl?: string
-) {
-  if (!redirectUrl) {
-    sendResponse({ type: AUTH_FAIL });
-    return;
-  }
-
-  if (chrome.runtime.lastError || redirectUrl.includes('callback?error=access_denied')) {
-    sendResponse({ type: AUTH_FAIL });
-    return;
-  }
-
-  const parsedUrl = new URLParse(redirectUrl);
-  const responseParameters = getResponseParametersFromHash(parsedUrl.hash);
-  const accessToken = responseParameters.get('access_token');
-  const state = responseParameters.get('state');
-
-  if (state !== STATE || !accessToken) {
-    sendResponse({ type: AUTH_FAIL });
-    return;
-  }
-
-  sendResponse({ type: AUTH_SUCCESS, payload: accessToken });
-}
-
-function getResponseParametersFromHash(hash: string): Map<string, string> {
-  const responseParamters = new Map<string, string>();
-  const hashWithoutPoundPrefix = hash.substring(1); 
-  const responsePairs = hashWithoutPoundPrefix.split('&');
-
-  for (const responsePair of responsePairs) {
-    const [key, value] = responsePair.split('=');
-    responseParamters.set(key, value);
-  }
-
-  return responseParamters;
 }
