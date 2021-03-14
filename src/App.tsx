015fb6d33ddb7cb8f57d46728125bb5f4c142d68
Saad <saadmasood96@hotmail.com>
2021-03-14 15:16:38 -0400

    handle auth while converting in Converter component

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,33 @@ import * as URLParse from "url-parse";
 const { useState, useEffect } = React;
 
 import Converter from "./Components/Converter/Converter";
-import Login from "./Components/Login/Login";
 import Header from "./Components/Header/Header";
 
 import './App.css';
 
 const App = () => {
-  const [token, setToken] = useState<string>('');
-  const [isSoundCloudTab, setSoundCloudTab] = useState<boolean>(false);
+  const [soundCloudPath, setSoundCloudPath] = useState<string>('');
  
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
-      console.log(currentTab?.url)
       if (!currentTab?.url) {
         return;
       }
+
       const url = new URLParse(currentTab.url);
       const { hostname, pathname } = url; /// match on path as well
+
       if (hostname.includes('soundcloud.com')) {
-        setSoundCloudTab(true)
-        return;
+        setSoundCloudPath(pathname);
       }
     }); 
   }, []);
 
   const renderAppContent = (): JSX.Element => {
-    if (isSoundCloudTab) {
-      return token ? <Converter /> : <Login setToken={setToken} />;
-    }
-    return <div className="visitSoundCloud">Please visit SoundCloud to use this extension.</div>;
+    return soundCloudPath ?
+      <Converter soundCloudPath={soundCloudPath}/> :
+      <div className="visitSoundCloud">Please visit SoundCloud to use this extension.</div>;
   };
 
   return (
--- src/Components/Converter/Converter.tsx
+++ src/Components/Converter/Converter.tsx
@@ -1,11 +1,61 @@
 import * as React from 'react';
+const { useState } = React;
+import Button from '@material-ui/core/Button';
 
-const Login = () => {
+import { AUTH_FAIL, AUTH_SUCCESS, LOGIN, Message } from '../../types';
+
+import './Converter.css';
+
+const LOGIN_FAIL_ERROR = 'There was an error connecting to your Spotify account. Please try again.'
+
+interface ConverterProps {
+  soundCloudPath: string;
+}
+
+const Converter = ({ soundCloudPath }: ConverterProps) => {
+  /// render Auth Fail error bar
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [spotifyToken, setSpotifyToken] = useState<string>('');
+
+  const onSpotifyConnect = (message: Message) => {
+    const { type, payload } = message;
+    switch (type) {
+      case AUTH_FAIL:
+        setErrorMessage(LOGIN_FAIL_ERROR);
+        break;
+      case AUTH_SUCCESS:
+        if (payload) {
+          setSpotifyToken(payload);
+          convertToSpotify(payload);
+        }
+        else {
+          setErrorMessage(LOGIN_FAIL_ERROR);
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  const convertToSpotify = (token?: string) => {
+    const authToken = token ?? spotifyToken;
+  };
+
+  const onConvertClick = () => {
+    if (!spotifyToken) {
+      chrome.runtime.sendMessage({ type: LOGIN }, onSpotifyConnect);
+      return;
+    }
+    convertToSpotify();
+  };
+
   return (
-    <div>
-      Converter!
-    </div>
+    <div className="login">
+      <Button className="loginButton" onClick={onConvertClick}>
+        Connect to Spotify
+      </Button>
+  </div>
   );
 };
 
-export default Login;
+export default Converter;
